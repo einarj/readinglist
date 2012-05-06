e5045a0150f3d57a97f910090a4ac5efb367b8cc
Einar Jonsson <ejonsson@handpoint.com>
2012-05-06 20:16:13 +0000

    Initial empty application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Readinglist::Application.config.secret_token = 'dc850d58b728a6871d9667f59f2bcf10309a8dac89fc31a8e61dce385c207a0babc773b64566e7d08717d04a7c4fee9cf2ea91bc46603f8323b195e3fc13da0d'
